04fb10f3fa5d1c58b84340f47381d55c809ad094
Stan <8138029+squiig@users.noreply.github.com>
2022-08-21 00:08:09 +0200

    Update 0.28 (camera rotation control)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField] private float _rotSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float speed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) ? _rotSpeed * 2 : _rotSpeed;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(Vector3.up * Time.deltaTime * speed);
+		}
+		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(Vector3.up * Time.deltaTime * -speed);
+		}
+	}
+}
